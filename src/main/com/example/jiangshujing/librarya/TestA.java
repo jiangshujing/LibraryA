d8923a90c107d50cff3d0724b508453ead52752e
jiangshujing <jiangshujing@teddymobile.cn>
2019-06-04 16:17:31 +0800

    TestA添加 set方法

--- src/main/com/example/jiangshujing/librarya/TestA.java
+++ src/main/com/example/jiangshujing/librarya/TestA.java
@@ -2,7 +2,13 @@ package com.example.jiangshujing.libraryb;
 
 public class TestA {
 
+    static String name;
+
     public static String get() {
         return "LibraryA";
+    }
+
+    public static void set(String name) {
+        TestA.name = name;
     }
 }

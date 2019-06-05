3db99415e499727ed5d3e2dbcb3c8827ae0093c6
jiangshujing <jiangshujing@teddymobile.cn>
2019-06-05 10:56:30 +0800

    liba 添加方法并添加 lib_base 依赖

--- src/main/java/com/example/jiangshujing/librarya/TestA.java
+++ src/main/java/com/example/jiangshujing/librarya/TestA.java
@@ -1,13 +1,13 @@
-package com.example.jiangshujing.libraryb;
+package com.example.jiangshujing.librarya;
 
 public class TestA {
 
-    //test git
     static String name;
 
     public static String get() {
-        return "LibraryA";
+        return name;
     }
+
 
     public static void set(String name) {
         TestA.name = name;
